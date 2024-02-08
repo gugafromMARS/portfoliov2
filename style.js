eae7438f3a0005735baf2da34389a98b185a0d60
Gonçalo <goncaloscravo@gmail.com>
2024-02-08 21:38:01 +0000

    Fix: another nav implementation

--- style.js
+++ style.js
@@ -4,6 +4,7 @@ const nav = document.querySelector(".nav");
 const navItems = document.querySelector(".nav__items");
 const logo = document.querySelector(".logo");
 const section1 = document.querySelector("#section--1");
+const firstBlock = document.querySelector(".first");
 
 navItems.addEventListener("click", (e) => {
   e.preventDefault();
@@ -32,11 +33,18 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 
 nav.addEventListener("mouseout", handleHover.bind(1));
 
-const section1Coords = section1.getBoundingClientRect();
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > section1Coords.top) {
-    console.log(section1Coords.top);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
     nav.classList.add("sticky-nav");
+  } else {
+    nav.classList.remove("sticky-nav");
   }
+};
+
+const firstBlockObs = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
 });
+
+firstBlockObs.observe(firstBlock);
